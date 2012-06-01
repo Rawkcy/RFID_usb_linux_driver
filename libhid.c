d3cfbb8e9d3d9eb2caa29c62fbf55df95d52cf1e
Roxanne Guo <roxane.guo@gmail.com>
2012-06-01 11:02:52 -0700

    will continue at home

--- libhid.c
+++ libhid.c
@@ -9,6 +9,77 @@
 #define PATHLEN 1
 
 
+static void RFIDCommunication(hid_return ret, HIDInterface** const hid)
+{
+  void append(char* str, char c)
+  {
+    int len = strlen(str);
+    str[len] = c;
+    str[len+1] = '\0';
+  }
+
+  int const PATH_IN[] = { 0xff000001 };
+  unsigned int SEND_PACKET_LEN;
+  unsigned int REPLY_PACKET_LENGTH;
+
+  printf("\n/* * * * * * * * BEGIN COMMUNICATION WITH RFID DEVICE * * * * * * * */\n");
+  
+  // Looping the communication channel
+  while (true) {
+    //XXX Exiting if user input is not a number
+    printf("\nSize of packet to be sent: ");
+    if (scanf("%d", &SEND_PACKET_LEN) != 1) { return; }
+    printf("Size of expected packet reply: ");
+    if (scanf("%d", &REPLY_PACKET_LENGTH) != 1) { return; }
+
+//    char PACKET[] = { 0x31, 0x03, 0x01 };
+    char PACKET[SEND_PACKET_LEN];
+    printf("\nPlease input packet data\n");
+    char input_hex[] = { };
+    int num_of_inputs;
+    num_of_inputs = 0;
+    do {
+      printf("Input %d: ", num_of_inputs+1);
+      // TODO:
+      // append the user input into PACKET[]
+      //
+//      gets(input_hex);
+//      strcpy(PACKET, input_hex);
+      append(PACKET, input_hex);
+      ++num_of_inputs;
+    } while (num_of_inputs < SEND_PACKET_LEN);
+    
+    // control transfer OUT from HOST-TO-DEVICE
+    ret = hid_set_output_report(*hid, PATH_IN, PATHLEN, PACKET, SEND_PACKET_LEN);
+    if (ret != HID_RET_SUCCESS) {
+      fprintf(stderr, "hid_set_output_report failed with return code %d\n", ret);
+    } else {
+      printf("\nhid_set_output_report successful!\n");
+      printf("\nsent: %s\n", PACKET);
+    }
+
+    // interrupt IN from DEVICE-TO-HOST
+    char reply[REPLY_PACKET_LENGTH];
+
+    fprintf(stderr, "trying to read from RFID\n");
+    ret = hid_interrupt_read(*hid, EP_INTERRUPT_IN, reply, REPLY_PACKET_LENGTH, 100);
+    if (ret != HID_RET_SUCCESS) {
+      fprintf(stderr, "interrupt read failed with return code %d\n", ret);
+    } else {
+      //TODO: Print reply
+      char * pch;
+      printf("\ninterrupt read successful!\n");
+      fprintf(stderr, "Parsing reply: %s\n", reply);
+      pch = strtok (reply, "\\x");
+      while (pch != NULL) {
+	fprintf(stderr, "%s\n", pch);
+	pch = strtok (NULL, "\\x");
+      }
+    }
+  }
+}
+
+
 int main(int argc, char *argv[])
 {
   HIDInterface* hid;
@@ -17,17 +88,14 @@ int main(int argc, char *argv[])
 
   unsigned short vendor_id  = 0x1325;
   unsigned short product_id = 0xc029;
-  unsigned char const REPLY_PACKET_LENGTH = 3;
-  unsigned char const SEND_PACKET_LEN = 3;
   char *vendor, *product;
   
   // Match USB by VID and PID
   HIDInterfaceMatcher matcher = { vendor_id, product_id, NULL, NULL, 0 };
 
-  /* see include/debug.h for possible values */
+  // Enable debugging
   hid_set_debug(HID_DEBUG_ALL);
   hid_set_debug_stream(stderr);
-  /* passed directly to libusb */
   hid_set_usb_debug(0);
   
 
@@ -48,6 +116,7 @@ int main(int argc, char *argv[])
     fprintf(stderr, "*** hid_new_HIDInterface success! ***\n");
   }
  
+  //TODO: Use hotplug or give permission to USB without sudo
   /* How to detach a device from the kernel HID driver:
    *
    * The hid.o or usbhid.ko kernel modules claim a HID device on insertion,
@@ -72,7 +141,7 @@ int main(int argc, char *argv[])
     fprintf(stderr, "hid_force_open failed with return code %d\n", ret);
     return 1;
   } else {
-    fprintf(stderr, "hid_forcE_open success!\n");
+    fprintf(stderr, "*** hid_forcE_open success! ***\n");
   }
  
   ret = hid_write_identification(stdout, hid);
@@ -80,7 +149,7 @@ int main(int argc, char *argv[])
     fprintf(stderr, "hid_write_identification failed with return code %d\n", ret);
     return 1;
   } else {
-    fprintf(stderr, "hid_write_identification success!\n");
+    fprintf(stderr, "*** hid_write_identification success! ***\n");
   }
  
   ret = hid_dump_tree(stdout, hid);
@@ -88,57 +157,18 @@ int main(int argc, char *argv[])
     fprintf(stderr, "hid_dump_tree failed with return code %d\n", ret);
     return 1;
   } else {
-    fprintf(stderr, "hid_dump_tree success!\n");
+    fprintf(stderr, "*** hid_dump_tree success! ***\n");
   }
  
+  RFIDCommunication(ret, &hid);
+  printf("\n/* * * * * * * * END COMMUNICATION WITH RFID DEVICE * * * * * * * */\n\n");
 
-  /* * * * * * * * BEGIN COMMUNICATION WITH RFID DEVICE * * * * * * * */
-  //TODO: create a polling loop here
-  //allow user to input the control transfer command
-  //poll for interrupt
-  //allow user to quit by hitting CR
-
-  // control transfer HOST-TO-DEVICE
-  int const PATH_IN[] = { 0xff000001 };
-  //char const PACKET[] = { 0xC0, 0x03, 0x12 };
-  char const PACKET[] = { 0x31, 0x03, 0x01 };
-  
-  ret = hid_set_output_report(hid, PATH_IN, PATHLEN, PACKET, SEND_PACKET_LEN);
-  if (ret != HID_RET_SUCCESS) {
-    fprintf(stderr, "hid_set_output_report failed with return code %d\n", ret);
-  } else {
-    fprintf(stderr, "hid_set_output_report successful!\n");
-  }
-
-  // interrupt receive DEVICE-TO-HOST
-  // TODO:REPLY_PACKET_LENGTH should be variable
-  char reply[REPLY_PACKET_LENGTH];
-
-  fprintf(stderr, "trying to read from RFID\n");
-  ret = hid_interrupt_read(hid, EP_INTERRUPT_IN, reply, REPLY_PACKET_LENGTH, 100);
-  if (ret != HID_RET_SUCCESS) {
-    fprintf(stderr, "interrupt read failed with return code %d\n", ret);
-    return 1;
-  } else {
-    char * pch;
-    fprintf(stderr, "interrupt read successful!\n");
-    fprintf(stderr, "Parsing reply: %s\n", reply);
-    pch = strtok (reply, "\\x");
-    while (pch != NULL) {
-      fprintf(stderr, "%s\n", pch);
-      pch = strtok (NULL, "\\x");
-    }
-  }
-  //TODO: Print reply
-  /* * * * * * * * END COMMUNICATION WITH RFID DEVICE * * * * * * * */
-  
- 
   ret = hid_close(hid);
   if (ret != HID_RET_SUCCESS) {
     fprintf(stderr, "hid_close failed with return code %d\n", ret);
     return 1;
   } else {
-    fprintf(stderr, "hid_close successful!\n");
+    fprintf(stderr, "*** hid_close successful! ***\n");
   }
  
   hid_delete_HIDInterface(&hid);
@@ -148,7 +178,7 @@ int main(int argc, char *argv[])
     fprintf(stderr, "hid_cleanup failed with return code %d\n", ret);
     return 1;
   } else {
-    fprintf(stderr, "hid_cleanup successful!\n");
+    fprintf(stderr, "*** hid_cleanup successful! ***\n");
   }
   
   return 0;
